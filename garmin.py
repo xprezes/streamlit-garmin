1da2c59195ebf4df1de6561f63144903fe2f1b4b
Maciej Błasiak <61689365+xprezes@users.noreply.github.com>
2023-01-30 10:41:25 +0100

    Update garmin.py

--- garmin.py
+++ garmin.py
@@ -26,6 +26,7 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
+            viewport {visibility: hidden;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
